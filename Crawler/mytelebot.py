70cd59a75db68efe4d6b5d58f88e6b7ebc9bd0d8
Auxesis <Jitender.Bhutani@AuxesisGroup.com>
2018-08-08 17:10:20 +0530

    Removing_test_data

--- Crawler/mytelebot.py
+++
@@ -1,8 +0,0 @@
-import telepot
-token = '650314389:AAHgry_aMyS4vpvpgErNNfHkVEwzpaSnHPU'
-tele_bot = telepot.Bot(token)
-print tele_bot.getMe()
-print tele_bot.getUpdates()
-# tele_bot.sendMessage('565177104','Hi')
-# tele_bot.sendMessage('669302591','Hi',reply_to_message_id=2,disable_notification=True)
-
